41d3c31d6e98798b937323c60572becfbe2cacff
Dieter Best <dieterbest@gmail.com>
2023-05-24 16:34:23 -0700

    add wdl script version to run pipeline

--- data/run.sh
+++ data/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#
+# run varpipe
+#
 FASTQ=$HOME/Analysis/wgs_pipeline/data/
 R1=$FASTQ/ERR552797_1.fastq.gz
 R2=$FASTQ/ERR552797_2.fastq.gz
@@ -8,12 +11,10 @@ R2=$FASTQ/ERR552797_30percent_2.fq.gz
 SAMPL=ERR552797
 REF=../tools/ref2.fa
 
-#cp ../tools/config.yml  
-###../tools/Varpipeline -q $R1 -q2 $R2 -r $REF -g NC_000962 -n $SAMPL -o $SAMPL -a -v -t 8 -k
-#exit 0
+#../tools/Varpipeline -q $R1 -q2 $R2 -r $REF -g NC_000962 -n $SAMPL -o $SAMPL -a -v -t 8 -k
 
 #
-# run Docker
+# run Docker varpipe
 #
 
 #TOOLS=/NCHHSTP-DTBE-Varpipe-WGS/tools
@@ -31,7 +32,12 @@ R2=$FSQ/ERR552797_30percent_2.fq.gz
 DATA=$HOME/Analysis/varpipe3/data
 WRK=/mnt/data
 
-docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe3:latest Varpipeline -q $R1 -q2 $R2 -r $REF -g NC__000962 -n $SAMPL -v -a -t 8 -k
+#docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe3:latest Varpipeline -q $R1 -q2 $R2 -r $REF -g NC__000962 -n $SAMPL -v -a -t 8 -k
+
+#
+# run WDL script varpipe
+#
+miniwdl run varpipe.wdl -i varpipe.yaml
 
 exit 0
 
