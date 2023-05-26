d1602a8f0ac67f60f2b14559ea6c4af65c40f3e0
Dieter Best <dieterbest@gmail.com>
2023-05-26 10:57:37 -0700

    call reference genome by its true name

--- data/run.sh
+++ data/run.sh
@@ -9,7 +9,7 @@ R2=$FASTQ/ERR552797_2.fastq.gz
 R1=$FASTQ/ERR552797_30percent_1.fq.gz
 R2=$FASTQ/ERR552797_30percent_2.fq.gz
 SAMPL=ERR552797
-REF=../tools/ref2.fa
+REF=../tools/NC_000962.3.fa
 
 #../tools/Varpipeline -q $R1 -q2 $R2 -r $REF -g NC_000962 -n $SAMPL -o $SAMPL -a -v -t 8 -k
 
@@ -20,7 +20,7 @@ REF=../tools/ref2.fa
 #TOOLS=/NCHHSTP-DTBE-Varpipe-WGS/tools
 TOOLS=$HOME/Analysis/varpipe/tools
 TLS=/mnt/tools
-REF=$TLS/ref2.fa
+REF=$TLS/NC_000962.3.fa
 
 FASTQ=$HOME/Analysis/wgs_pipeline/data/
 FSQ=/mnt/fastq
@@ -32,7 +32,7 @@ R2=$FSQ/ERR552797_30percent_2.fq.gz
 DATA=$HOME/Analysis/varpipe3/data
 WRK=/mnt/data
 
-#docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe3:latest Varpipeline -q $R1 -q2 $R2 -r $REF -g NC__000962 -n $SAMPL -v -a -t 8 -k
+#docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe3:latest Varpipeline -q $R1 -q2 $R2 -r $REF -g NC_000962 -n $SAMPL -v -a -t 8 -k
 
 #
 # run WDL script varpipe
