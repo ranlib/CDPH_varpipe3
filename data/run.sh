e5cd818f2e2ffce6fa0dcc935fa05a8737a7b959
Dieter Best <dieterbest@gmail.com>
2023-06-04 12:55:24 -0700

    add flag to run.sh to be able to choose to run in conda, docker, or wdl mode

--- data/run.sh
+++ data/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] ; then
+    echo "Usage: run.sh <MODE>"
+    echo "MODE = conda | docker | wdl"
+    exit 1
+fi
+MODE=$1
+
 #
 # run varpipe
 #
@@ -13,16 +20,16 @@ SAMPL=ERR552797
 REF=../tools/NC_000962.3.fa
 NAME="NC_000962.3"
 
-../tools/Varpipeline -q $R1 -q2 $R2 -r $REF -g $NAME -n $SAMPL -o $SAMPL -a -v -t 8 -k
-
-exit
+if [ "$MODE" = "conda" ] ; then
+   ../tools/Varpipeline -q $R1 -q2 $R2 -r $REF -g $NAME -n $SAMPL -o $SAMPL -a -v -t 8 -k
+fi
 
 #
 # run Docker varpipe
 #
 
 #TOOLS=/NCHHSTP-DTBE-Varpipe-WGS/tools
-TOOLS=$HOME/Analysis/varpipe/tools
+TOOLS=$HOME/Analysis/varpipe3/tools
 TLS=/mnt/tools
 REF=$TLS/NC_000962.3.fa
 
@@ -36,12 +43,16 @@ R2=$FSQ/ERR552797_30percent_2.fq.gz
 DATA=$HOME/Analysis/varpipe3/data
 WRK=/mnt/data
 
-#docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe3:latest Varpipeline -q $R1 -q2 $R2 -r $REF -g $NAME -n $SAMPL -v -a -t 8 -k
+if [ "$MODE" = "docker" ] ; then
+   docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe3:latest Varpipeline -q $R1 -q2 $R2 -r $REF -g $NAME -n $SAMPL -v -a -t 8 -k
+fi
 
 #
 # run WDL script varpipe
 #
-miniwdl run varpipe.wdl -i varpipe.yaml
+if [ "$MODE" = "wdl" ] ; then
+   miniwdl run varpipe.wdl -i varpipe.yaml
+fi
 
 exit 0
 
